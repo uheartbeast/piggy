a45a769d08e77aaa4a80b87b326184aae1e4a798
Benjamin Anderson <heartbeast.studios@gmail.com>
2019-09-02 16:03:38 -0600

    Delta + Export Variables

--- Pig.gd
+++ Pig.gd
@@ -1,17 +1,17 @@
 extends Area2D
 
-const SPEED = 4
+export(int) var SPEED = 50
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
-		move(SPEED, 0)
+		move(SPEED, 0, delta)
 	if Input.is_action_pressed("ui_left"):
-		move(-SPEED, 0)
+		move(-SPEED, 0, delta)
 	if Input.is_action_pressed("ui_up"):
-		move(0, -SPEED)
+		move(0, -SPEED, delta)
 	if Input.is_action_pressed("ui_down"):
-		move(0, SPEED)
+		move(0, SPEED, delta)
 
-func move(xspeed, yspeed):
-	position.x += xspeed
-	position.y += yspeed
+func move(xspeed, yspeed, delta):
+	position.x += xspeed * delta
+	position.y += yspeed * delta
